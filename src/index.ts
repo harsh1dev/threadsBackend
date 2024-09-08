6030e8aefb8d05f5318bc9cb0a08e2d0cf05007c
harsh1dev <harshkumar1.dev@gmail.com>
2024-09-08 17:07:33 +0530

    added graphql

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import express from 'express';
-
-
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
 const app = express();
 
-const PORT  = Number(process.env.PORT) || 3001;
+const PORT = Number(process.env.PORT) || 3001;
+
+async function startServer() {
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+        type Query {
+            hello: String
+        }`,
+        resolvers: {
+            Query:{
+                hello: () => 'Hello, World!',
+            }
+        }
+    });
+
+    await gqlServer.start();
+    app.use(express.json());
+    app.use('/graphql',expressMiddleware(gqlServer));
+    app.listen(PORT, () => console.log('App listening on port  ', PORT));
+}
+
+startServer();
 
 
-app.listen(PORT, ()=> console.log('App listening on port  ', PORT));

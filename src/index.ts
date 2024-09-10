23f2c4425de52212cfa49b38a5968990b4bfc1e2
harsh1dev <harshkumar1.dev@gmail.com>
2024-09-10 22:16:07 +0530

    refectored code

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,15 @@
 import express from 'express';
-import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { prismaClient } from './lib/db';
+import createApolloGraphqlServer from "./graphql";
 const app = express();
 
 const PORT = Number(process.env.PORT) || 3001;
 
 async function startServer() {
-    const gqlServer = new ApolloServer({
-        typeDefs: `
-        type Query {
-            hello: String
-            say(name: String): String
-        }
-        type Mutation{
-            createUser(firstName: String, lastName: String, email: String,password: String):Boolean
-        
-        
-        }
-        `,
-        resolvers: {
-            Query:{
-                hello: () => 'Hello, World!',
-                say: (_,{name}:{name:string}) => `Hi ${name}! How are you`
-            },
-            Mutation:{
-                createUser: async (_, {firstName, lastName, email, password}) => {
-                    try {
-                        await prismaClient.user.create({
-                            data: {
-                                firstName,
-                                lastName,
-                                email,
-                                password,
-                                salt: "salt"
-                            }
-                        });
-                        return true;
-                    } catch (error) {
-                        console.error(error);
-                        return false;
-                    }
-                }
-            }
-        }
-    });
+    
 
-    await gqlServer.start();
     app.use(express.json());
-    app.use('/graphql',expressMiddleware(gqlServer));
+    app.use('/graphql',expressMiddleware(await createApolloGraphqlServer()));
     app.listen(PORT, () => console.log('App listening on port  ', PORT));
 }
 
